9e61f5fa2682182845c7ffd3844a03401a29eaba
Мария Харламова <maria.h4rlamova@yandex.ru>
2024-11-05 17:37:58 +0300

    added video player

---
+++ src/VideoPlayer.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+import ReactPlayer from "react-player";
+import { Button, Flex, Modal } from "antd";
+
+import { useMachine } from "@xstate/react";
+import {
+  CompressOutlined,
+  ExpandOutlined,
+  PauseCircleOutlined,
+  PlayCircleOutlined,
+} from "@ant-design/icons";
+
+import cls from "./VideoPlayer.module.css";
+import videoPlayerMachine from "./videoPlayerMachine";
+
+const VideoPlayer = () => {
+  const [state, send] = useMachine(videoPlayerMachine);
+
+  return (
+    <Flex className={cls.content}>
+      <Button
+        className={cls.openButton}
+        color='primary'
+        variant='outlined'
+        onClick={() => send({ type: "OPEN" })}
+      >
+        <PlayCircleOutlined className={cls.openButton__icon} />
+      </Button>
+      <Modal
+        open={state.matches("control.open")}
+        onCancel={() => send({ type: "CLOSE" })}
+        footer={null}
+        width={state.matches("visibility.minimized") ? 300 : 800}
+      >
+        <ReactPlayer
+          url='https://cdn.flowplayer.com/d9cd469f-14fc-4b7b-a7f6-ccbfa755dcb8/hls/383f752a-cbd1-4691-a73f-a4e583391b3d/playlist.m3u8'
+          playing={state.matches("playback.playing")}
+          width='100%'
+          height='100%'
+        />
+        <Flex className={cls.buttonsContainer}>
+          <Button
+            onClick={() =>
+              send(
+                state.matches("visibility.normal")
+                  ? { type: "MINIMIZE" }
+                  : { type: "MAXIMIZE" }
+              )
+            }
+            icon={
+              state.matches("visibility.normal") ? (
+                <CompressOutlined />
+              ) : (
+                <ExpandOutlined />
+              )
+            }
+            shape='circle'
+          />
+          <Button
+            onClick={() =>
+              send(
+                state.matches("playback.playing")
+                  ? { type: "PAUSE" }
+                  : { type: "PLAY" }
+              )
+            }
+            shape='circle'
+            icon={
+              state.matches("playback.playing") ? (
+                <PauseCircleOutlined />
+              ) : (
+                <PlayCircleOutlined />
+              )
+            }
+          />
+        </Flex>
+      </Modal>
+    </Flex>
+  );
+};
+
+export default VideoPlayer;
